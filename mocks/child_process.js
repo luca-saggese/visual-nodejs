97a7d4811b36b64fd38f97486568054ba629352e
Luca Saggese <luca.saggese@gmail.com>
2025-11-19 11:35:21 +0100

    wip

--- mocks/child_process.js
+++ mocks/child_process.js
@@ -1,10 +1,35 @@
+const EventEmitter = require('events');
+
+class MockStream extends EventEmitter {}
+class MockProcess extends EventEmitter {
+  constructor() {
+    super();
+    this.stdout = new MockStream();
+    this.stderr = new MockStream();
+    this.pid = Math.floor(Math.random() * 10000);
+    
+    setTimeout(() => {
+        this.stdout.emit('data', 'Visual Node.js Runtime (Mock Environment)\n');
+    }, 100);
+    
+    setTimeout(() => {
+        this.stdout.emit('data', 'Running in React Native macOS...\n');
+        this.stdout.emit('data', 'Note: Native Node.js spawning is mocked in this prototype.\n');
+    }, 500);
+  }
+  
+  kill() {
+      this.emit('exit', 0);
+  }
+}
+
 module.exports = {
-  spawn: () => ({
-    pid: 123,
-    stdout: { on: () => {} },
-    stderr: { on: () => {} },
-    on: () => {},
-    kill: () => {}
-  }),
-  exec: () => {}
+  spawn: (command, args, options) => {
+    console.log('Mock spawn:', command, args);
+    return new MockProcess();
+  },
+  exec: (command, callback) => {
+      console.log('Mock exec:', command);
+      if (callback) callback(null, 'Mock Output', '');
+  }
 };
